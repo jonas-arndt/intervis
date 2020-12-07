f9d8cebf22c1b0f4f16e9dfd234c6932217423ec
Jonas Rogge <jonasrogge@jr-work.fritz.box>
2020-12-07 15:02:09 +0100

    Add first styling for case studies

--- components/chapters/CaseStudies.vue
+++ components/chapters/CaseStudies.vue
@@ -3,21 +3,21 @@
     <div class="step">
       <h2>Fallbeispiele aus Deutschland</h2>
       <p>
-        Kurzer Text: Lokale Fallbeispiel zur Veranschaulichung. Hier genannten Beispiele sind nur ein kleiner Teil. Du kannst dir aussuchen mit welchem Beispiel du anfangen möchtest. Lorem ipsum dolor sit amet, consectetuer.
+        Zur Veranschaulichung der Auswirkungen von intersektionaler Diskriminierung schauen wir uns lokale Fallbeispiele aus Deutschland an. Die folgenden Beispiele sind ein kleiner Ausschnitt der zahlreichen, intersektionalen Diskriminierungsformen und -erfahrungen. Wähle eine Beispiel, worüber du mehr erfahren möchtest.
       </p>
     </div>
 
-    <div class="case-study-1">
+    <div class="case-study">
       <div class="case-study-link">
         <a href="#">Frauen* mit Kopftuch beim Zugang zum Arbeitsmarkt</a>
       </div>
     </div>
-    <div class="case-study-2">
+    <div class="case-study">
       <div class="case-study-link">
         <a href="#">Sinti*zze und Rom*nja Kinder im Schulsystem</a>
       </div>
     </div>
-    <div class="case-study-3">
+    <div class="case-study">
       <div class="case-study-link">
         <a href="#">Frauen* mit Behinderung auf dem Arbeitsmarkt</a>
       </div>
@@ -30,6 +30,73 @@
 
 .chapter.case_studies_from_germany {
   position: relative;
+  padding: 20vh 10% 0 10%;
+
+  height: 100vh;
+
+  h2 {
+    color: $color-red;
+    margin-bottom: 0.5em;
+  }
+
+  p {
+    margin-bottom: 3em;
+    width: 50%;
+    font-size: $font-size-small;
+  }
+
+  .case-study {
+    position: relative;
+    display: inline-block;
+    width: 22%;
+    margin-right: 16%;
+
+    vertical-align: top;
+
+    &:last-child {
+      margin-right: inherit;
+    }
+  }
+
+  .case-study-link {
+    position: relative;
+    height: 10rem;
+
+    padding: 0.25rem 0.75rem 0.25rem 0.75rem;
+
+    $border-definition: 2px solid $color-red;
+
+    &:before, &:after {
+      content: "";
+
+      position: absolute;
+      width: 1em;
+      height: 1em;
+    }
+
+    &:before {
+      top: 0;
+      left: 0;
+
+      border-top: $border-definition;
+      border-left: $border-definition;
+    }
+
+    &:after {
+      right: 0;
+      bottom: 0;
+
+      border-right: $border-definition;
+      border-bottom: $border-definition;
+    }
+
+    a {
+      font-family: $font-family-signika;
+      font-size: $font-size-subtitle;
+      color: $color-black;
+
+    }
+  }
 
   &:after {
     content: "";
