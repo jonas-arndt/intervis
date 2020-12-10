0576ae01dcd92af9061245b717bca33459d303ad
Jonas Rogge <jonasrogge@jr-work.fritz.box>
2020-12-10 16:47:34 +0100

    Add component for grid visualization

---
+++ components/visualizations/GridVisualization.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="grid-visualization-component">
+    lines
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    step: {
+      type: Number,
+      default: 0
+    },
+    progress: {
+      type: Number,
+      default: 0
+    }
+  },
+  data () {},
+  mounted () {}
+}
+</script>
+
+<style scoped lang="scss">
+.grid-visualization-component {}
+</style>
