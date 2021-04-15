6fdf26338f937088da3a90c1e5b73a8cc15bc139
Jonas Arndt <jonas.rogge@gmx.de>
2021-04-15 09:19:42 +0200

    Add interactions to teaser buttons

--- components/chapters/Teaser.vue
+++ components/chapters/Teaser.vue
@@ -12,14 +12,25 @@
           Ziel dieses Artikels ist es die Themen Intersektionalität, Diskriminierung und Privilegierung mithilfe visueller Elemente anschaulich zu erklären. Intersektionale Perspektiven sind bislang nicht im Alltagswortschatz und in gesellschaftlichen Diskursen in Deutschland angekommen. Das möchten wir ändern, dieser Artikel kann ein Anfang dafür sein. Eine erste Version des Artikels entstand im Rahmen der Design Bachelorarbeit von Hannah Schwan. Anschließend wurde er im UCLAB von Hannah, Jonas Arndt, Sandra Cartes und Marian Dörk weiterentwickelt.
         </p>
         <ButtonLine>
-          <RedButton>Zum Artikel</RedButton>
-          <RedButton>Mehr Einblicke ins Projekt</RedButton>
+          <button class="red-button" @click="hideTeaser()">
+            Zum Artikel
+          </button>
+          <button class="red-button" @click="toggleDisclosureVisibility()">
+            Mehr Einblicke ins Projekt
+          </button>
         </ButtonLine>
       </div>
     </div>
   </div>
 </template>
 
-<style lang="scss" scoped>
+<script>
+import { mapMutations } from 'vuex'
 
-</style>
+export default {
+  methods: {
+    ...mapMutations(['hideTeaser', 'toggleDisclosureVisibility'])
+  }
+}
+
+</script>
--- components/elements/RedButton.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <button class="red-button">
-    <slot>RedButton</slot>
-  </button>
-</template>
