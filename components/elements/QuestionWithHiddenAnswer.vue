87ef8e61675304a90213b6daa63bbf8529c47268
Jonas Arndt <jonas.rogge@gmx.de>
2021-04-15 11:59:38 +0200

    Add “question with hidden answer” component

---
+++ components/elements/QuestionWithHiddenAnswer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="hidden-content-block">
+    <p>
+      <slot></slot>
+    </p>
+    <div class="hidden-content-wrapper">
+      <span><slot name="toggle"></slot></span>
+      <div class="hidden-content">
+        <slot name="content"></slot>
+      </div>
+    </div>
+  </div>
+</template>
