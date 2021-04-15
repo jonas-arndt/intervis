35ec2a3c8dd58a22f388dfce9e53b4bb3e8b2281
Jonas Arndt <jonas.rogge@gmx.de>
2021-04-15 11:46:55 +0200

    Add design decision contents

---
+++ components/design-decisions/DesignDecisions3.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>Visualisierung von Daten zu intersektionaler Diskriminierung</h1>
+
+    <QuestionWithHiddenAnswer>
+      Über die Daten
+      <template v-slot:toggle>Mehr dazu</template>
+      <template v-slot:answer>
+        <p>
+          Der Mangel an verfügbaren intersektionalen Antidiskriminierungs- und Gleich- stellungsdaten erschwert es hervorzuheben, wie sich intersektionale Diskri-minierung speziell auf verschiedene Bevölkerungsgruppen auswirkt. Kombinationen von qualitativen und quantitativen Daten sowie vorhandene, stell- vertretende Daten können verwendet werden, um Tendenzen aufzuzeigen. Die hier verwendeten Daten sind Teil von umfangreichen Umfragen, Studien und Berichten zu Diskriminierung in Deutschland und sind lediglich ein kleiner Ausschnitt von diesen. Auch die Umfragen und Studien selber sind partiell und bilden ebenfalls einen kleinen Ausschnitt der zahlreichen Diskriminierungsformen- und erfahrungen. Sie zeigen aber dennoch wichtige und interessante Tendenzen. Ziel und Zweck dieser Studien und Berichte ist die Offenlegung von bestimmten Diskriminierungsformen als Beweis und Grundlage für notwendige Antidiskri- minierungsmaßnahmen und -handlungen. Innerhalb dieser Studien und Berichte werden Datenerhebungs- und analysemethoden, Kontexte der Daten etc. beschrieben, auf die wir hier verweisen möchten:
+        </p>
+        <p>
+          Innerhalb der → Studie zur aktuellen Bildungssituation deutscher Sinti und Roma. Dokumentation und Forschungsbericht werden Daten Methoden und Kontexte auf S.4ff (Einleitung), S. 7ff (Methodische Herausforderungen) und S. 17ff (Auswertung von quantitativen Daten zur Erhebung) beschrieben. Die in diesem Artikel verwendeten Daten lassen sich auf S. 101f finden.
+        </p>
+        <p>
+          Innerhalb des Feldexperiments → Discrimination against Female Migrants Wearing Headscarves werden Daten Methoden und Kontexte ab S. 7 (Current study) beschrieben. Die in diesem Artikel verwendeten Daten lassen sich auf S. 22ff finden.
+        </p>
+        <p>
+          Innerhalb der Studie → Lebenssituation und Belastungen von Frauen mit Beeinträchtigungen und Behinderungen in Deutschland. Kurzfassung werden Daten Methoden und Kontexte auf S. 9f (Methodische Anlage der Studie) beschrieben. Die in diesem Artikel verwendeten Daten lassen sich auf S. 21 &amp; 24 finden.
+        </p>
+        <p>
+          Innerhalb des Berichtes → Zweiter Teilhabebericht der Bundesregierung über die Lebenslagen von Menschen mit Beeinträchtigungen werden Daten Methoden und Kontexte auf S. 533ff (In eigener Sache: Daten, Methoden und Literatur) beschrieben. Die in diesem Artikel verwendeten Daten lassen sich auf S. 170 finden (die Primärquelle dieser Daten ist der Mikrozensus 2013; im Rahmen des Berichts wurde die Analyse der Daten vom Institut für Sozialforschung und Gesellschaftspolitik durchgeführt).
+        </p>
+      </template>
+    </QuestionWithHiddenAnswer>
+
+    <QuestionWithHiddenAnswer>
+      Für die Visualisierung von Daten aus Studien werden keine standardisierten und konventionellen Visualisierungs formen genutzt
+      <template v-slot:toggle>Warum?</template>
+      <template v-slot:answer>Lorem Ipsum …</template>
+    </QuestionWithHiddenAnswer>
+
+    <QuestionWithHiddenAnswer>
+      Die verwendeten Daten werden kontextualisiert
+      <template v-slot:toggle>Warum?</template>
+      <template v-slot:answer>Lorem Ipsum …</template>
+    </QuestionWithHiddenAnswer>
+</template>
