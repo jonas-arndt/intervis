6c8f09c63e2748fab1d2e581921a71fe2b4b5ef0
Jonas Arndt <jonas.rogge@gmx.de>
2021-04-16 14:27:46 +0200

    Fix content

--- components/design-decisions/DesignDecisions3.vue
+++ components/design-decisions/DesignDecisions3.vue
@@ -3,33 +3,33 @@
     <h1>{{ $t('slidein3-subtitle') }}</h1>
 
     <QuestionWithHiddenAnswer>
-      <h1>{{ $t('slidein3-heading1') }}</h1>
+      {{ $t('slidein3-heading1') }}
       <template v-slot:toggle>
-        <h1>{{ $t('slidein3-button1') }}</h1>
+        {{ $t('slidein3-button1') }}
       </template>
       <template v-slot:answer>
-        <p /><h1>{{ $t('slidein3-text1') }}</h1>
+        <p />{{ $t('slidein3-text1') }}
         </p>
       </template>
     </QuestionWithHiddenAnswer>
 
     <QuestionWithHiddenAnswer>
-      <h1>{{ $t('slidein3-heading2') }}</h1>
+      {{ $t('slidein3-heading2') }}
       <template v-slot:toggle>
-        <h1>{{ $t('slidein3-button2') }}</h1>
+        {{ $t('slidein3-button2') }}
       </template>
       <template v-slot:answer>
-        <h1>{{ $t('slidein3-text2') }}</h1>
+        {{ $t('slidein3-text2') }}
       </template>
     </QuestionWithHiddenAnswer>
 
     <QuestionWithHiddenAnswer>
-      <h1>{{ $t('slidein3-heading3') }}</h1>
+      {{ $t('slidein3-heading3') }}
       <template v-slot:toggle>
-        <h1>{{ $t('slidein3-button3') }}</h1>
+        {{ $t('slidein3-button3') }}
       </template>
       <template v-slot:answer>
-        <h1>{{ $t('slidein3-text3') }}</h1>
+        {{ $t('slidein3-text3') }}
       </template>
     </QuestionWithHiddenAnswer>
   </div>
