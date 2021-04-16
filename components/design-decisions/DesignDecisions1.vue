71e460756df7da67aee24d89f18a98ffcddedf43
Jonas Arndt <jonas.rogge@gmx.de>
2021-04-16 11:46:34 +0200

    Add multilanguage support for design decisions

--- components/design-decisions/DesignDecisions1.vue
+++ components/design-decisions/DesignDecisions1.vue
@@ -1,41 +1,41 @@
 <template>
   <div>
-    <h1>Visualisierung von Intersektionalität</h1>
+    <h1>{{ $t('slidein1-subtitle') }}</h1>
     <p class="teaser">
-      Die Besonderheit und Komplexität von Intersektionalität liegt unter anderem darin, dass sie sowohl die Eigenständigkeit, als auch den stetigen Zusammenhang, das stetige Zusammenwirken von Macht- und Herrschaftsstrukturen erfasst. Eine visuelle Repräsentation von Intersektionalität muss dies widerspiegeln, um daraufhin mithilfe dieser visuellen Sprache, diese Besonderheit und Komplexität wiederum vermitteln zu können.
+      {{ $t('slidein1-intro') }}
     </p>
 
     <QuestionWithHiddenAnswer>
-      Verschiedene Ausrichtungen von Strichen repräsentieren verschiedene soziale Kategorien
+      {{ $t('slidein1-heading1') }}
       <template v-slot:toggle>
-        Warum?
+        {{ $t('slidein1-button1') }}
       </template>
       <template v-slot:answer>
         <p>
-          Verschiedene Ausrichtungen von Strichen repräsentieren verschiedene soziale Kategorien. Es gibt eine Vielzahl an möglichen Ausrichtungsmöglichkeiten für eine Vielzahl von sozialen Kategorien. Die in diesem Artikel gezeigten Strichausrichtungen bzw. soziale Kategorien dienen als Beispiele und sind eine unvollständige Auswahl. Die »selbst gezeichneten«, unperfekten Striche sollen verdeutlichen, dass gesellschaftlich konstruierte soziale Kategorien nicht endgültig oder starr, sondern flexibel, kontextabhängig, von vielen verschiedenen Faktoren geprägt und in ständiger Veränderung sind. Ebenfalls soll diese Gestaltung der Striche suggerieren, dass die Visualisierungen nicht von vornherein gegeben und objektiv, sondern von Personen mit partiellen und situierten Positionen entwickelt worden sind.
+          {{ $t('slidein1-text1') }}
         </p>
       </template>
     </QuestionWithHiddenAnswer>
 
     <QuestionWithHiddenAnswer>
-      Intersektionalität wird durch ineinander verwobene Striche repräsentiert
+      {{ $t('slidein1-heading2') }}
       <template v-slot:toggle>
-        Warum?
+        {{ $t('slidein1-button2') }}
       </template>
       <template v-slot:answer>
-        Lorem Ipsum …
+        {{ $t('slidein1-text2') }}
       </template>
     </QuestionWithHiddenAnswer>
 
     <QuestionWithHiddenAnswer>
-      Eine verworfene Alternative
+      {{ $t('slidein1-heading3') }}
       <template v-slot:toggle>
-        Mehr dazu
+        {{ $t('slidein1-button3') }}
       </template>
       <template v-slot:answer>
         <img src="~/assets/intervis_disclosure_figures/alternative1.png">
         <p>
-          Designprozesse sind unter anderem davon geprägt, dass bestimmte entwickelte Ideen und Konzepte ganz oder teilweise verworfen werden. Ein Beispiel für einen verworfenen Ansatz in diesem Projekt ist die Idee, Intersektionalität mit Visualisierungsparametern der Deckkraft und Überlagerung zu visualisieren. Verschiedenfarbige Flächen, die sich überlagern, repräsentieren dabei verschiedene soziale Kategorien. Die Deckkraft markiert, ob eine Person oder Gruppe basierend auf der jeweiligen sozialen Kategorie diskriminiert (sehr hohe Deckkraft) oder privilegiert (sehr niedrige Deckkraft) ist, und ob somit das Lesen der darunter liegenden Begriffe erschwert wird oder nicht. Diese Art der Repräsentation wurde verworfen, da das Aufeinanderlegen von farbigen Flächen möglicherweise zu sehr eine additive Verbindung der sozialen Kategorien suggeriert und sie visuell noch zu stark für sich alleine stehen und wirken. Die Idee durch visuelle Mittel das Lesen von Begriffen zu erschweren, wurde jedoch in weiteren Visualisierungsansätzen angewandt.
+          {{ $t('slidein1-text3') }}
         </p>
       </template>
     </QuestionWithHiddenAnswer>
--- components/design-decisions/DesignDecisions3.vue
+++ components/design-decisions/DesignDecisions3.vue
@@ -1,48 +1,35 @@
 <template>
   <div>
-    <h1>Visualisierung von Daten zu intersektionaler Diskriminierung</h1>
+    <h1>{{ $t('slidein3-subtitle') }}</h1>
 
     <QuestionWithHiddenAnswer>
-      Über die Daten
+      <h1>{{ $t('slidein3-heading1') }}</h1>
       <template v-slot:toggle>
-        Mehr dazu
+        <h1>{{ $t('slidein3-button1') }}</h1>
       </template>
       <template v-slot:answer>
-        <p>
-          Der Mangel an verfügbaren intersektionalen Antidiskriminierungs- und Gleichstellungsdaten erschwert es hervorzuheben, wie sich intersektionale Diskriminierung speziell auf verschiedene Bevölkerungsgruppen auswirkt. Kombinationen von qualitativen und quantitativen Daten sowie vorhandene, stellvertretende Daten können verwendet werden, um Tendenzen aufzuzeigen. Die hier verwendeten Daten sind Teil von umfangreichen Umfragen, Studien und Berichten zu Diskriminierung in Deutschland und sind lediglich ein kleiner Ausschnitt von diesen. Auch die Umfragen und Studien selber sind partiell und bilden ebenfalls einen kleinen Ausschnitt der zahlreichen Diskriminierungsformen- und erfahrungen. Sie zeigen aber dennoch wichtige und interessante Tendenzen. Ziel und Zweck dieser Studien und Berichte ist die Offenlegung von bestimmten Diskriminierungsformen als Beweis und Grundlage für notwendige Antidiskriminierungsmaßnahmen und -handlungen. Innerhalb dieser Studien und Berichte werden Datenerhebungs- und analysemethoden, Kontexte der Daten etc. beschrieben, auf die wir hier verweisen möchten:
-        </p>
-        <p>
-          Innerhalb der → Studie zur aktuellen Bildungssituation deutscher Sinti und Roma. Dokumentation und Forschungsbericht werden Daten Methoden und Kontexte auf S.4ff (Einleitung), S. 7ff (Methodische Herausforderungen) und S. 17ff (Auswertung von quantitativen Daten zur Erhebung) beschrieben. Die in diesem Artikel verwendeten Daten lassen sich auf S. 101f finden.
-        </p>
-        <p>
-          Innerhalb des Feldexperiments → Discrimination against Female Migrants Wearing Headscarves werden Daten Methoden und Kontexte ab S. 7 (Current study) beschrieben. Die in diesem Artikel verwendeten Daten lassen sich auf S. 22ff finden.
-        </p>
-        <p>
-          Innerhalb der Studie → Lebenssituation und Belastungen von Frauen mit Beeinträchtigungen und Behinderungen in Deutschland. Kurzfassung werden Daten Methoden und Kontexte auf S. 9f (Methodische Anlage der Studie) beschrieben. Die in diesem Artikel verwendeten Daten lassen sich auf S. 21 &amp; 24 finden.
-        </p>
-        <p>
-          Innerhalb des Berichtes → Zweiter Teilhabebericht der Bundesregierung über die Lebenslagen von Menschen mit Beeinträchtigungen werden Daten Methoden und Kontexte auf S. 533ff (In eigener Sache: Daten, Methoden und Literatur) beschrieben. Die in diesem Artikel verwendeten Daten lassen sich auf S. 170 finden (die Primärquelle dieser Daten ist der Mikrozensus 2013; im Rahmen des Berichts wurde die Analyse der Daten vom Institut für Sozialforschung und Gesellschaftspolitik durchgeführt).
+        <p /><h1>{{ $t('slidein3-text1') }}</h1>
         </p>
       </template>
     </QuestionWithHiddenAnswer>
 
     <QuestionWithHiddenAnswer>
-      Für die Visualisierung von Daten aus Studien werden keine standardisierten und konventionellen Visualisierungsformen genutzt
+      <h1>{{ $t('slidein3-heading2') }}</h1>
       <template v-slot:toggle>
-        Warum?
+        <h1>{{ $t('slidein3-button2') }}</h1>
       </template>
       <template v-slot:answer>
-        Lorem Ipsum …
+        <h1>{{ $t('slidein3-text2') }}</h1>
       </template>
     </QuestionWithHiddenAnswer>
 
     <QuestionWithHiddenAnswer>
-      Die verwendeten Daten werden kontextualisiert
+      <h1>{{ $t('slidein3-heading3') }}</h1>
       <template v-slot:toggle>
-        Warum?
+        <h1>{{ $t('slidein3-button3') }}</h1>
       </template>
       <template v-slot:answer>
-        Lorem Ipsum …
+        <h1>{{ $t('slidein3-text3') }}</h1>
       </template>
     </QuestionWithHiddenAnswer>
   </div>
