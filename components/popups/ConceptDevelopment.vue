9cfaf8763e7eb99c212e20c7b5d24553e70fb9a8
Jonas Arndt <jonas.rogge@gmx.de>
2021-04-16 12:19:46 +0200

    Add multilanguage support for overlays

--- components/popups/ConceptDevelopment.vue
+++ components/popups/ConceptDevelopment.vue
@@ -1,61 +1,58 @@
 <template>
   <div class="chapter concept-development horizontal-content">
     <h2 class="chapter-title">
-      Begriffsentwicklung von Intersektionalität
+      {{ $t('overlay1-title') }}
     </h2>
     <div class="content">
       <div class="block">
         <p class="text teaser">
-          Die Entstehung von Intersektionalitätstheorien verlief nicht geradlinig. Sie ist geprägt von vielfältigen Entwicklungslinien. Und wie jede Historie, ist sie ihrerseits von Marginalisierung und Ausschlüssen geprägt. Innerhalb dieses Artikels wird die Begriffsentwicklung im US-amerikanischen und deutschen Kontext in groben Zügen skizziert.
+          {{ $t('overlay1-intro') }}
         </p>
       </div>
 
       <div class="block">
         <div class="title usa icon">
           <h3 class="label">
-            USA
+            {{ $t('overlay1-subtitle1') }}
           </h3>
         </div>
         <h4 class="block-title">
-          <q>Ain’t I a woman?</q>
+          <q>{{ $t('overlay1-heading1') }}</q>
         </h4>
         <p class="text">
-          1851 fragte Sojourner Truth in ihrer Rede auf der Frauenkonferenz in Ohio in das Publikum: »Ain’t I a woman?«, bin ich keine Frau? Wenngleich der Begriff der Intersektionalität erst später geprägt wurde, wiesen sie und weitere Sklavinnen bereits im 19. Jahrhundert auf ihre doppelt marginalisierte Position als Frauen* und Sklavinnen/Schwarze hin und thematisierten, wie sie mehrere, gleichzeitige zusammenwirkende Unterdrückungen erfuhren.
+          {{ $t('overlay1-text1') }}
         </p>
       </div>
 
       <div class="block columned">
         <h4 class="block-title">
-          Vielfältige Theoriebildung
+          {{ $t('overlay1-heading2') }}
         </h4>
         <p class="text">
-          Im Zuge der Bewegung des »Black Feminism« in den USA formten sich in den 1970er und 1980er Jahren verstärkte Diskurse und Debatten, in denen Schwarzen Frauen* ihre spezifischen Erfahrungen in einer rassistischen, patriarchalen, heterosexistischen und kapitalistischen Gesellschaft thematisierten. Ein zentraler Kritikpunkt galt der gängigen feministischen Forschung und Politik, da diese sich an den Interessen weißer, westlicher, heterosexueller, cis-geschlechtlicher Frauen aus der Mittelschicht ausrichtete. Damit wurden die Erfahrungen, Bedürfnisse und Interessen von z.B. Schwarzen Frauen* ignoriert, marginalisiert und letztendlich unsichtbar gemacht. Dies wird auch als »white feminism« bezeichnet.
-          <br>Die Theoriebildung der Intersektionalität wurde in diesem Kontext auf unterschiedliche Weise von vielen Autor*innen geprägt und bereichert.
+          {{ $t('overlay1-text2') }}
         </p>
       </div>
 
       <div class="block">
         <h4 class="block-title">
-          Kimberlé Crenshaw
+          {{ $t('overlay1-heading3') }}
         </h4>
         <p class="text">
-          1989 führte die US-amerikanische Juristin und Professorin für Critical Legal Studies Kimberlé Crenshaw in ihrem Essay <q>Demarginalizing the Intersection of Race and Sex: A Black Feminist Critique of Antidiscrimination Doctrine, Feminist Theory and Antiracist Politics</q> erstmals den Begriff <q>Intersektionalität</q> ein.
+          {{ $t('overlay1-text3') }}
         </p>
       </div>
 
       <div class="block columned">
         <div class="title germany icon">
           <h3 class="label">
-            Deutschland
+            {{ $t('overlay1-subtitle2') }}
           </h3>
         </div>
         <h4 class="block-title">
-          Kritik am Mainstream-Feminismus
+          {{ $t('overlay1-heading4') }}
         </h4>
         <p class="text">
-          Die Entwicklungslinien deutschsprachiger Intersektionalitätstheorien sind vielfältiger und gebrochener, verglichen mit denen der angloamerikanischen Entwicklung.
-          <br>Politisch-theoretische Debatten wurden teilweise durch US-amerikanische Texte beeinflusst, teilweise aber auch unabhängig davon geführt.
-          <br>Impulsgeber*innen in den 1980er und 1990er Jahren waren Migrant*innen, jüdische Frauen*, Schwarze Deutsche, Frauen* mit Behinderung und Queer of Color. Sie alle machten auf ihre spezifischen Diskriminierungserfahrungen und deren Marginalisierung im deutschen Mainstream-Feminismus aufmerksam. Sie thematisierten, dass sie als <q>die Anderen</q> repräsentiert und ihre Themen und Forderungen nicht wahrgenommen und berücksichtigt wurden. Dabei kritisierten sie vor allem den deutschen Mainstream-Feminismus, dessen Akteur*innen für sich beanspruchten, Politik für alle Frauen* zu betreiben, obwohl sie sich nur an den Interessen und Bedürfnissen weißer, cis-geschlechtlicher, christlicher, nichtbehinderter Frauen orientierten.
+          {{ $t('overlay1-text4') }}
         </p>
       </div>
     </div>
--- components/popups/DimensionsDiscrimination.vue
+++ components/popups/DimensionsDiscrimination.vue
@@ -1,58 +1,55 @@
 <template>
   <div class="chapter dimensions-of-discrimination horizontal-content">
     <h2 class="chapter-title">
-      Dimensionen von Diskriminierung
+      {{ $t('overlay2-title') }}
     </h2>
     <div class="content">
       <div class="block">
         <h3 class="block-title">
-          Direkt &amp; Indirekt
+          {{ $t('overlay2-subtitle1') }}
         </h3>
         <p class="text">
-          Direkte/unmittelbare Diskriminierung liegt nach Definition des Allgemeinen Gleichbehandlungsgesetzes (AGG) dann vor, <q>wenn eine Person wegen eines in § 1 genannten Grundes [›Rasse‹, ethnische Herkunft, Geschlecht, Religion oder Weltanschauung, Behinderung, Alter oder sexuelle Identität] eine weniger günstige Behandlung erfährt, als eine andere Person in einer vergleichbaren Situation erfährt, erfahren hat oder erfahren würde</q> (§ 3 (1) AGG). Indirekte/mittelbare Diskriminierung liegt dann vor, <q>wenn dem Anschein nach neutrale Vorschriften, Kriterien oder Verfahren Personen wegen eines in § 1 genannten Grundes gegenüber anderen Personen in besonderer Weise benachteiligen können</q> (§ 3 (2) AGG).
+          {{ $t('overlay2-text1') }}
         </p>
       </div>
 
       <div class="block">
         <img class="icon" :src="require('~/assets/icons/dimensions/dimension_individuell.svg')">
         <h3 class="block-title">
-          Individuelle Dimension
+          {{ $t('overlay2-subtitle2') }}
         </h3>
         <p class="text">
-          Die individuelle Dimension von Diskriminierung ist die Dimension, die den meisten Menschen bekannt ist. Eine Person diskriminiert – absichtlich oder unabsichtlich – eine andere. Täter*in(nen) und Opfer können benannt werden. Beispiele dafür sind Hate Speech, Vorurteile, verbale und körperliche Gewalt.
-          <br>Die individuelle Dimension ist jedoch nur die Spitze des Eisbergs.
+          {{ $t('overlay2-text2') }}
         </p>
       </div>
 
       <div class="block">
         <img class="icon" :src="require('~/assets/icons/dimensions/dimension_strukturell.svg')">
         <h3 class="block-title">
-          Strukturelle Dimension
+          {{ $t('overlay2-subtitle3') }}
         </h3>
         <p class="text">
-          Die strukturelle Dimension von Diskriminierung kommt bei der statistischen Anhäufung von einzelnen Diskriminierungserfahrungen zum Vorschein und repräsentiert die soziale und strukturelle Ungleichheit, die durch Diskriminierung und Privilegierung manifestiert wird. Diese spiegelt sich in allen gesellschaftlichen Institutionen wider: auf dem Arbeitsmarkt, auf dem Wohnungsmarkt, im Schulsystem, im Gesundheitswesen etc.
-          <br>So sind zum Beispiel spezifische Gruppen in bestimmten Sektoren oder Institutionen (z.B. in Förderschulen, in Gefängnissen, in bestimmten Arbeitssektoren) über- oder unterrepräsentiert. Diese Statistiken können falsch interpretiert oder sogar instrumentalisiert werden, wenn die strukturelle Dimension von Diskriminierung nicht mit einbezogen wird, indem Über- oder Unterrepräsentationen zum Beispiel mit vermeintlichen Eigenschaften einer bestimmten Gruppe verbunden werden. Auch die Argumentation der <q>Umgekehrten Diskriminierung</q> ignoriert die strukturelle Dimension.
+          {{ $t('overlay2-text3') }}
         </p>
       </div>
 
       <div class="block">
         <img class="icon" :src="require('~/assets/icons/dimensions/dimension_institutionell.svg')">
         <h3 class="block-title">
-          Institutionelle Dimension
+          {{ $t('overlay2-subtitle4') }}
         </h3>
         <p class="text">
-          Diese Dimension von Diskriminierung manifestiert sich durch vermeintlich neutrale Gesetze und Politiken, durch die jedoch bestimmte Personen und/oder Gruppen diskriminiert werden.
-          <br>Sie trägt zur Aufrechterhaltung von ungleichen Machtverhältnissen durch Gesetze und Institutionen bei. Diejenigen, die sich in Institutionen in einer Machtpositionen befinden, z.B. Lehrer*innen, Richter*innen, Polizist*innen, reproduzieren und bekräftigen – meist unabsichtlich – Diskriminierung in ihren Handlungen, Vorgehensweisen und Entscheidungen durch unbewusste Vorurteile und Stereotypisierungen. Ein Beispiel dafür ist <q>Racial Profiling</q>.
+          {{ $t('overlay2-text4') }}
         </p>
       </div>
 
       <div class="block">
         <img class="icon" :src="require('~/assets/icons/dimensions/dimension_historisch.svg')">
         <h3 class="block-title">
-          Historische Dimension
+          {{ $t('overlay2-subtitle5') }}
         </h3>
         <p class="text">
-          Die historische Dimension von Diskriminierung ist die Basis der drei anderen Dimensionen. Historische Ereignisse haben immer Einfluss auf die Gegenwart. So beeinflussen beispielsweise die Versklavung afrikanischer Menschen, der Kolonialismus und der Holocaust/Porajmos heutige Diskriminierungs- und Marginalisierungsstrukturen und bestimmen weiterhin, wie Personen und Gruppen hierarchisiert und Macht und Ressourcen verteilt sind.
+          {{ $t('overlay2-text5') }}
         </p>
       </div>
     </div>
