2a455d001b1ebd8b9981d91ad35b576767db8330
Jonas Arndt <jonas.rogge@gmx.de>
2021-04-16 10:10:10 +0200

    Add project disclosure content

---
+++ components/disclosure/ProjectDisclosurePart2.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="disclosure content">
+    <h1>Visualisierungsentscheidungen &amp; Datenkontext</h1>
+    <h2>Visualisierung von Intersektionalität</h2>
+    <p>
+      Die Besonderheit und Komplexität von Intersektionalität liegt unter anderem darin, dass sie sowohl die Eigenständigkeit, als auch den stetigen Zusammenhang, das stetige Zusammenwirken von diversen sozialen Kategorien bzw. Machtverhältnissen erfasst. Eine visuelle Repräsentation von Intersektionalität muss dies widerspiegeln, um diese Besonderheit und Komplexität vermitteln zu können.
+    </p>
+
+    <QuestionWithHiddenAnswer>
+      Verschiedene Ausrichtungen von Strichen repräsentieren verschiedene soziale Kategorien
+      <template v-slot:toggle>
+        Warum?
+      </template>
+      <template v-slot:answer>
+        Lorem Ipsum …
+      </template>
+    </QuestionWithHiddenAnswer>
+
+    <QuestionWithHiddenAnswer>
+      Intersektionalität wird durch ineinander verwobene Striche repräsentiert
+      <template v-slot:toggle>
+        Warum?
+      </template>
+      <template v-slot:answer>
+        Lorem Ipsum …
+      </template>
+    </QuestionWithHiddenAnswer>
+
+    <QuestionWithHiddenAnswer>
+      Eine verworfene Alternative
+      <template v-slot:toggle>
+        Mehr dazu
+      </template>
+      <template v-slot:answer>
+        <img src="~/assets/images/disclosure/alternative-drafts-1.png">
+        <img src="~/assets/images/disclosure/alternative-drafts-2.png">
+        <p>
+          Designprozesse sind unter anderem davon geprägt, dass bestimmte entwickelte Ideen und Konzepte ganz oder teilweise verworfen werden. Ein Beispiel für einen verworfenen Ansatz in diesem Projekt ist die Idee, Intersektionalität mit Visualisierungsparametern der Deckkraft und Überlagerung zu visualisieren. Verschiedenfarbige Flächen, die sich überlagern, repräsentieren dabei verschiedene soziale Kategorien. Die Deckkraft markiert, ob eine Person oder Gruppe basierend auf der jeweiligen sozialen Kategorie diskriminiert (sehr hohe Deckkraft) oder privilegiert (sehr niedrige Deckkraft) ist, und ob somit das Lesen der darunter liegenden Begriffe erschwert wird oder nicht. Diese Art der Repräsentation wurde verworfen, da das Aufeinanderlegen von farbigen Flächen möglicherweise zu sehr eine additive Verbindung der sozialen Kategorien suggeriert und sie visuell noch zu stark für sich alleine stehen und wirken. Die Idee durch visuelle Mittel das Lesen von Begriffen zu erschweren, wurde jedoch in weiteren Visualisierungsansätzen angewandt.
+        </p>
+      </template>
+    </QuestionWithHiddenAnswer>
+
+    <h2>Visualisierung von Diskriminierung und Privilegierung</h2>
+
+    <QuestionWithHiddenAnswer>
+      Das Netz aus verwobenen Strichen ist immer sichtbar und einzelne Strichausrichtungen werden nie ohne dieses Netz dargestellt
+      <template v-slot:toggle>
+        Warum?
+      </template>
+      <template v-slot:answer>
+        Lorem Ipsum …
+      </template>
+    </QuestionWithHiddenAnswer>
+
+    <QuestionWithHiddenAnswer>
+      Diskriminierung wird durch orangefarbene und in den Vordergrund tretende Striche visualisiert
+      <template v-slot:toggle>
+        Warum?
+      </template>
+      <template v-slot:answer>
+        <p>
+          Diskriminierung wird durch orangefarbene und in den Vordergrund tretende Striche visualisiert. Sie erschweren das Lesen der Begriffe Zugang, Einbeziehung, Teilhabe und Anerkennung bzw. können sie sogar unlesbar machen: Personen, die Diskriminierung erfahren, sind benachteiligt, wenn es um Zugang zu Ressourcen, soziale und politische Teilhabe, gesellschaftliche Einbeziehung und Anerkennung etc. geht. Privilegierung wird durch grau-violette Striche visualisiert, die im Hintergrund verweilen. Sie hindern nicht am Lesen der Begriffe. Mit der Verwendung dieser übereinanderliegenden Ebenen soll die vermeintlich »normale« – neutrale, objektive – Sicht auf die Wörter herausgefordert werden. Denn dieser »view from nowhere« ist immer ein »view from somewhere«, nämlich die Sicht von denjenigen, die in unserer Gesellschaft privilegiert sind und als Norm gelten.
+        </p>
+      </template>
+    </QuestionWithHiddenAnswer>
+
+    <QuestionWithHiddenAnswer>
+      Es können sich vielfältige, unterschiedliche und individuelle Formen aus dem Netz der verwobenen Striche herausbilden
+      <template v-slot:toggle>
+        Warum?
+      </template>
+      <template v-slot:answer>
+        Lorem Ipsum …
+      </template>
+    </QuestionWithHiddenAnswer>
+
+    <QuestionWithHiddenAnswer>
+      Eine verworfene Alternative
+      <template v-slot:toggle>
+        Mehr dazu
+      </template>
+      <template v-slot:answer>
+        <p>
+          Ein*e im Projektprozess befragte*r Expert*in zu Intersektionalität wies darauf hin, dass soziale Kategorien aus intersektionaler Perspektive nie isoliert voneinander zu betrachten sind. Das Netz der sozialen Kategorien – unabhängig davon, ob eine Person basierend auf diesen sozialen Kategorien diskriminiert oder privilegiert wird – ist immer und für jede Person rekonstruierbar und jede Person ist durch mehrere Identitäten intersektional geprägt. Personen oder Gruppen, die »nur« sexistische Diskriminierung erfahren und in anderen sozialen Kategorien privilegiert sind, mit ausschließlich horizontalen Strichen zu visualisieren (was Diskriminierung basierend auf der sozialen Kategorie »Geschlecht« angzeigt) ist daher nicht ganz treffend und kann zu einem falschen Verständnis von Intersektionalität führen. Diese sehr hilfreiche Kritik führte zu einer Verfeinerung der visuellen Repräsentation: Das Netz der sozialen Kategorien ist immer sichtbar. Diskriminierung wird durch orangefarbene und in den Vordergrund tretende Striche visualisiert. Privilegierung wird durch grau-violette Striche visualisiert, die im Hintergrund verweilen.
+        </p>
+      </template>
+    </QuestionWithHiddenAnswer>
+
+    <h2>Visualisierung von Daten zu intersektionaler Diskriminierung</h2>
+
+    <QuestionWithHiddenAnswer>
+      Über die Daten
+      <template v-slot:toggle>
+        Mehr dazu
+      </template>
+      <template v-slot:answer>
+        <p>
+          Der Mangel an verfügbaren intersektionalen Antidiskriminierungs- und Gleichstellungsdaten erschwert es hervorzuheben, wie sich intersektionale Diskriminierung speziell auf verschiedene Bevölkerungsgruppen auswirkt. Kombinationen von qualitativen und quantitativen Daten sowie vorhandene, stellvertretende Daten können verwendet werden, um Tendenzen aufzuzeigen.
+        </p>
+        <p>
+          Die hier verwendeten Daten sind Teil von umfangreichen Umfragen, Studien und Berichten zu Diskriminierung in Deutschland und sind lediglich ein kleiner Ausschnitt von diesen. Auch die Umfragen und Studien selber sind partiell und bilden ebenfalls einen kleinen Ausschnitt der zahlreichen Diskriminierungsformen- und erfahrungen. Sie zeigen aber dennoch wichtige und interessante Tendenzen. Ziel und Zweck dieser Studien und Berichte ist die Offenlegung von bestimmten Diskriminierungsformen als Beweis und Grundlage für notwendige Antidiskriminierungsmaßnahmen und -handlungen. Innerhalb dieser Studien und Berichte werden Datenerhebungs- und analysemethoden, Kontexte der Daten etc. beschrieben, auf die wir hier verweisen möchten:
+        </p>
+        <p>
+          Innerhalb der → Studie zur aktuellen Bildungssituation deutscher Sinti und Roma. Dokumentation und Forschungsbericht werden Daten Methoden und Kontexte auf S.4ff (Einleitung), S. 7ff (Methodische Herausforderungen) und S. 17ff (Auswertung von quantitativen Daten zur Erhebung) beschrieben. Die in diesem Artikel verwendeten Daten lassen sich auf S. 101f finden.
+        </p>
+        <p>
+          Innerhalb des Feldexperiments → Discrimination against Female Migrants Wearing Headscarves werden Daten Methoden und Kontexte ab S. 7 (Current study) beschrieben. Die in diesem Artikel verwendeten Daten lassen sich auf S. 22ff finden.
+        </p>
+        <p>
+          Innerhalb der Studie → Lebenssituation und Belastungen von Frauen mit Beeinträchtigungen und Behinderungen in Deutschland. Kurzfassung werden Daten Methoden und Kontexte auf S. 9f (Methodische Anlage der Studie) beschrieben. Die in diesem Artikel verwendeten Daten lassen sich auf S. 21 &amp; 24 finden.
+        </p>
+        <p>
+          Innerhalb des Berichtes → Zweiter Teilhabebericht der Bundesregierung über die Lebenslagen von Menschen mit Beeinträchtigungen werden Daten Methoden und Kontexte auf S. 533ff (In eigener Sache: Daten, Methoden und Literatur) beschrieben. Die in diesem Artikel verwendeten Daten lassen sich auf S. 170 finden (die Primärquelle dieser Daten ist der Mikrozensus 2013; im Rahmen des Berichts wurde die Analyse der Daten vom Institut für Sozialforschung und Gesellschaftspolitik durchgeführt).
+        </p>
+      </template>
+    </QuestionWithHiddenAnswer>
+
+    <QuestionWithHiddenAnswer>
+      Für die Visualisierung von Daten aus Studien werden keine standardisierten und konventionellen Visualisierungsformen genutzt
+      <template v-slot:toggle>
+        Warum?
+      </template>
+      <template v-slot:answer>
+        Lorem Ipsum …
+      </template>
+    </QuestionWithHiddenAnswer>
+
+    <QuestionWithHiddenAnswer>
+      Die verwendeten Daten werden kontextualisiert
+      <template v-slot:toggle>
+        Warum?
+      </template>
+      <template v-slot:answer>
+        Lorem Ipsum …
+      </template>
+    </QuestionWithHiddenAnswer>
+  </div>
+</template>
---
+++ components/disclosure/ProjectDisclosurePart4.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="disclosure content">
+    <h1>Unsere Positionen</h1>
+    <p>
+      Dieses Projekt ist geprägt von Personen mit bestimmten Standpunkten und Positionen in der Welt innerhalb bestimmter sozialer, politischer, ökonomischer und kultureller Kontexte. Um unsere eigenen partiellen und situierten Positionen anzuerkennen, legen wir sie offen. Wer sind wir? Was machen wir? Was ist unsere berufliche, soziale, politische Position? Was ist unsere Motivation für dieses Projekt?
+    </p>
+
+    <h2>Hannah</h2>
+    <p>
+      Ich bin Masterstudentin im Bereich Design an der Fachhochschule Potsdam, wo ich 2020 auch meinen Bachelorabschluss erlangt habe. Mein Fokus liegt hauptsächlich auf Informationsvisualisierung und Interfacedesign. Während des Bachelorstudiums habe ich zudem begonnen, mich mit Feminismus und Intersektionalität zu beschäftigen und bin immer weiter in diese Themen eingestiegen, bis sie mich nicht mehr losgelassen haben. Zum einen, weil sie einige persönliche Erfahrungen und Empfindungen erklärt und in einen Kontext gesetzt haben. Zum anderen, weil sie für mich als sehr privilegierte Person viele gesellschaftliche Phänomene und Probleme erklärt haben, die ich davor nicht gesehen habe, den großen Handlungsbedarf aufgedeckt und das Bewusstsein und die Anerkennung geschaffen haben, dass ich Teil dieser Probleme bin und damit Verantwortung bei notwendigen Veränderungen übernehmen muss.
+      <br>Meine Bachelorarbeit war das erste größere Projekt, in welchem ich diese zwei Interessenbereiche von Design/Visualisierung und intersektionalem Feminismus verbunden habe. Die erste Version dieses Artikels entstand innerhalb meiner Bachelorarbeit. Zurzeit bin ich studentische Mitarbeiterin UCLAB, wo ich dieses Projekt zusammen mit tollen Menschen weiterentwickelt durfte.
+      <br>Mit diesem Artikel möchte ich die Themen Intersektionalität, Diskriminierung und Privilegierung zugänglich und anschaulich machen, dazu beitragen, dass Leser*innen neue Erkenntnisse und Perspektiven erlangen und sie ermutigen, sich weiter mit diesen Themen zu beschäftigen.
+    </p>
+
+    <h2>Jonas</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore.
+    </p>
+
+    <h2>Sandra</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore.
+    </p>
+
+    <h2>Marian</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore.
+    </p>
+  </div>
+</template>
