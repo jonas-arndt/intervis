14be5ad7953db641642ac273b7692d9804e8aea6
Jonas Arndt <jonas.rogge@gmx.de>
2021-04-20 10:29:50 +0200

    Add images and real content to blob visualizations

--- components/visualizations/WorkingHandicappedVisualization.vue
+++ components/visualizations/WorkingHandicappedVisualization.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="working-handicapped-visualization">
-    <img :src="require('~/assets/visualizations/statistic-working-handicapped.jpg')">
+    <div class="visualization-1">
+      <img :src="require('~/assets/fallback/case-studies/statistic-working-handicapped-1.png')">
+      <div class="legend">
+        <span class="digit" v-html="$t('chapter3-example3-vis3')" />
+        <span class="details" v-html="$t('chapter3-example3-vis4')" />
+      </div>
+    </div>
+    <div class="visualization-2">
+      <img :src="require('~/assets/fallback/case-studies/statistic-working-handicapped-2.png')">
+      <div class="legend">
+        <span class="digit" v-html="$t('chapter3-example3-vis5')" />
+        <span class="details" v-html="$t('chapter3-example3-vis6')" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,23 +24,62 @@
   position: relative;
   height: 100vh;
 
-  img {
+  .legend {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+
+    span {
+      display: block;
+    }
+
+    .digit {
+      font-size: $font-size-title;
+      font-style: italic;
+      font-weight: 600;
+      margin-bottom: 1rem;
+    }
+
+    .details {
+      font-size: $font-size-small;
+    }
+  }
+
+  .visualization-1 {
     position: relative;
-    display: inline-block;
-    vertical-align: middle;
-    height: 100%;
-    top: 5vh;
-    left: 1rem;
-    max-width: 90%;
-    max-height: 90vh;
+    width: 85%;
+
+    left: 5%;
+    top: 7%;
+    padding-bottom: 2rem;
+
+    img {
+      position: relative;
+      max-width: 50%;
+      left: 30%;
+    }
+  }
+
+  .visualization-2 {
+    position: absolute;
+    width: 85%;
+
+    left: 5%;
+    bottom: 5%;
+
+    img {
+      position: relative;
+      max-width: 80%;
+    }
+
+    .legend {
+      margin-bottom: 1rem;
+    }
   }
 
   @media (min-width: $media-breakpoint-min-m) {
-    img {
-      position: absolute;
-      right: 5%;
-      left: auto;
-    }
+    left: 45%;
+    width: 50%;
   }
 }
 </style>
--- components/visualizations/WorkingWomanVisualization.vue
+++ components/visualizations/WorkingWomanVisualization.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="working-woman-visualization">
-    <img :src="require('~/assets/visualizations/statistic-working-women.jpg')">
+    <div class="visualization-1">
+      <img :src="require('~/assets/fallback/case-studies/statistic-working-women-1.png')">
+      <div class="legend">
+        <span class="digit" v-html="$t('chapter3-exampl1-vis1')" />
+        <span class="details" v-html="$t('chapter3-exampl1-vis2')" />
+      </div>
+    </div>
+    <div class="visualization-2">
+      <img :src="require('~/assets/fallback/case-studies/statistic-working-women-2.png')">
+      <div class="legend">
+        <span class="digit" v-html="$t('chapter3-exampl1-vis3')" />
+        <span class="details" v-html="$t('chapter3-exampl1-vis4')" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,23 +24,62 @@
   position: relative;
   height: 100vh;
 
-  img {
+  .legend {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+
+    span {
+      display: block;
+    }
+
+    .digit {
+      font-size: $font-size-title;
+      font-style: italic;
+      font-weight: 600;
+      margin-bottom: 1rem;
+    }
+
+    .details {
+      font-size: $font-size-small;
+    }
+  }
+
+  .visualization-1 {
     position: relative;
-    display: inline-block;
-    vertical-align: middle;
-    height: 100%;
-    top: 5vh;
-    left: 1rem;
-    max-width: 90%;
-    max-height: 90vh;
+    width: 85%;
+
+    left: 5%;
+    top: 7%;
+    padding-bottom: 2rem;
+
+    img {
+      position: relative;
+      max-width: 90%;
+    }
+  }
+
+  .visualization-2 {
+    position: absolute;
+    width: 85%;
+
+    left: 5%;
+    bottom: 17%;
+
+    img {
+      position: relative;
+      max-width: 30%;
+      left: 30%;
+    }
+
+    .legend {
+      margin-bottom: 1rem;
+    }
   }
 
   @media (min-width: $media-breakpoint-min-m) {
-    img {
-      position: absolute;
-      right: 5%;
-      left: auto;
-    }
+    left: 45%;
+    width: 50%;
   }
 }
 </style>
