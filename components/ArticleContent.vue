65b4833e8200c847ea9dc3b4dbf6b5d091441f85
Jonas Arndt <jonas.rogge@gmx.de>
2021-04-22 11:48:39 +0200

    Fix scrollama initilization issue

--- components/ArticleContent.vue
+++ components/ArticleContent.vue
@@ -2,20 +2,19 @@
   <div class="article-content">
     <div class="content-with-background">
       <div class="background">
-        <LineVisualization />
+        <LineVisualization @hook:mounted="setComponentMounted('LineVisualization')" />
       </div>
       <div class="foreground">
-        <Title />
-        <Introduction />
-        <Intersectionality />
-        <DiscriminationAndPrivilege />
-        <CaseStudies />
-        <Measures />
+        <Title @hook:mounted="setComponentMounted('Title')" />
+        <Introduction @hook:mounted="setComponentMounted('Introduction')" />
+        <Intersectionality @hook:mounted="setComponentMounted('Intersectionality')" />
+        <DiscriminationAndPrivilege @hook:mounted="setComponentMounted('DiscriminationAndPrivilege')" />
+        <CaseStudies @hook:mounted="setComponentMounted('CaseStudies')" />
+        <Measures @hook:mounted="setComponentMounted('Measures')" />
       </div>
     </div>
-    <NextSteps />
-    <Sources />
-  </div>
+    <NextSteps @hook:mounted="setComponentMounted('NextSteps')" />
+    <Sources @hook:mounted="setComponentMounted('Sources')" />
   </div>
 </template>
 
@@ -24,30 +23,69 @@ import scrollama from 'scrollama'
 
 export default {
   data () {
+    const subComponents = [
+      'LineVisualization',
+      'Title',
+      'Introduction',
+      'Intersectionality',
+      'DiscriminationAndPrivilege',
+      'CaseStudies',
+      'Measures',
+      'NextSteps',
+      'Sources'
+    ]
+
+    const mountedSubComponents = {}
+    for (const componentName of subComponents) {
+      mountedSubComponents[componentName] = false
+    }
+
     return {
-      scroller: scrollama()
+      mountedSubComponents,
+      scroller: undefined
     }
   },
-  mounted () {
-    // setup the instance, pass callback functions
-    this.scroller
-      .setup({
-        step: '.step',
+  computed: {
+    subComponentsFullyMounted () {
+      for (const componentName in this.mountedSubComponents) {
+        if (!this.mountedSubComponents[componentName]) {
+          return false
+        }
+      }
+      return true
+    }
+  },
+  watch: {
+    subComponentsFullyMounted (subComponentsFullyMounted) {
+      if (subComponentsFullyMounted) {
+        this.initScrollama()
+      }
+    }
+  },
+  methods: {
+    setComponentMounted (componentName) {
+      this.mountedSubComponents[componentName] = true
+    },
+    initScrollama () {
+      this.scroller = scrollama()
+      this.scroller.setup({
+        step: '.chapter',
         progress: true,
-        debug: true
+        debug: false
       })
-      .onStepEnter((response) => {
-        // { element, index, direction }
-      })
-      .onStepExit((response) => {
-        // { element, index, direction }
-      })
-      .onStepProgress((response) => {
-        // { element: DOMElement, index: number, progress: number }
-      })
+        .onStepEnter((response) => {
+          // { element, index, direction }
+        })
+        .onStepExit((response) => {
+          // { element, index, direction }
+        })
+        .onStepProgress((response) => {
+          // { element: DOMElement, index: number, progress: number }
+        })
 
-    // setup resize event
-    window.addEventListener('resize', this.scroller.resize)
+      // setup resize event
+      window.addEventListener('resize', this.scroller.resize)
+    }
   }
 }
 </script>
