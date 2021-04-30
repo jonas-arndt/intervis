268671e4dbba49c674963236c4bc15969736b4d2
Jonas Arndt <jonas.rogge@gmx.de>
2021-04-30 22:10:13 +0200

    Remove additional mounted logic

--- components/ArticleContent.vue
+++ components/ArticleContent.vue
@@ -2,67 +2,35 @@
   <div class="article-content">
     <div class="content-with-background">
       <div class="background">
-        <LineVisualization @hook:mounted="setComponentMounted('LineVisualization')" />
+        <LineVisualization />
       </div>
       <div class="foreground">
-        <Title @hook:mounted="setComponentMounted('Title')" />
-        <Introduction @hook:mounted="setComponentMounted('Introduction')" />
-        <Intersectionality @hook:mounted="setComponentMounted('Intersectionality')" />
-        <DiscriminationAndPrivilege @hook:mounted="setComponentMounted('DiscriminationAndPrivilege')" />
-        <CaseStudies @hook:mounted="setComponentMounted('CaseStudies')" />
-        <Measures @hook:mounted="setComponentMounted('Measures')" />
+        <Title />
+        <Introduction />
+        <Intersectionality />
+        <DiscriminationAndPrivilege />
+        <CaseStudies />
+        <Measures />
       </div>
     </div>
-    <NextSteps @hook:mounted="setComponentMounted('NextSteps')" />
-    <ProjectInformation @hook:mounted="setComponentMounted('ProjectInformation')" />
-    <References @hook:mounted="setComponentMounted('References')" />
+    <NextSteps />
+    <ProjectInformation />
+    <References />
   </div>
 </template>
 
 <script>
 export default {
   data () {
-    const subComponents = [
-      'LineVisualization',
-      'Title',
-      'Introduction',
-      'Intersectionality',
-      'DiscriminationAndPrivilege',
-      'CaseStudies',
-      'Measures',
-      'NextSteps',
-      'ProjectInformation',
-      'References'
-    ]
-
-    const mountedSubComponents = {}
-    for (const componentName of subComponents) {
-      mountedSubComponents[componentName] = false
-    }
-
     return {
-      mountedSubComponents,
     }
   },
   computed: {
-    subComponentsFullyMounted () {
-      for (const componentName in this.mountedSubComponents) {
-        if (!this.mountedSubComponents[componentName]) {
-          return false
-        }
-      }
-      return true
-    }
   },
   watch: {
-    subComponentsFullyMounted (subComponentsFullyMounted) {
-      if (subComponentsFullyMounted) {
-      }
     }
   },
   methods: {
-    setComponentMounted (componentName) {
-      this.mountedSubComponents[componentName] = true
     }
   }
 }
