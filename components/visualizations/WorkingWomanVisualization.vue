caa9a1527f39bd7baa3af7063cf8130ece7b018e
Jonas Arndt <jonas.rogge@gmx.de>
2021-05-10 11:44:52 +0200

    Update shapes of working women visualization

--- components/visualizations/WorkingWomanVisualization.vue
+++ components/visualizations/WorkingWomanVisualization.vue
@@ -19,7 +19,9 @@
       </div>
     </div>
     <div class="step-2">
-      <img class="shape" src="~/assets/fallback/chapter3_example1_quote.png">
+      <div class="shape">
+        <img src="~/assets/fallback/chapter3_example1_quote.png">
+      </div>
       <div class="quote">
         <p class="text" v-html="$t('chapter3-example1-quote')" />
       </div>
@@ -44,6 +46,38 @@
       height: 100vh;
       width: 100%;
     }
+
+    .visualization-1 {
+      position: relative;
+      width: 85%;
+
+      left: 5%;
+      top: 7%;
+      padding-bottom: 2rem;
+
+      img {
+        position: relative;
+        width: (1648% / 20);
+      }
+    }
+
+    .visualization-2 {
+      position: absolute;
+      width: 85%;
+
+      left: 5%;
+      bottom: 17%;
+
+      img {
+        position: relative;
+        width: (685% / 20);
+        left: 30%;
+      }
+
+      .legend {
+        margin-bottom: 1rem;
+      }
+    }
   }
 
   .step-2 {
@@ -60,6 +94,12 @@
       width: 100%;
       z-index: -1;
       opacity: 0.5;
+
+      img {
+        position: absolute;
+        width: 80%;
+        bottom: 10%;
+      }
     }
 
     .quote {
@@ -100,38 +140,6 @@
 
     .details {
       font-size: $font-size-small;
-    }
-  }
-
-  .visualization-1 {
-    position: relative;
-    width: 85%;
-
-    left: 5%;
-    top: 7%;
-    padding-bottom: 2rem;
-
-    img {
-      position: relative;
-      max-width: 90%;
-    }
-  }
-
-  .visualization-2 {
-    position: absolute;
-    width: 85%;
-
-    left: 5%;
-    bottom: 17%;
-
-    img {
-      position: relative;
-      max-width: 30%;
-      left: 30%;
-    }
-
-    .legend {
-      margin-bottom: 1rem;
     }
   }
 
