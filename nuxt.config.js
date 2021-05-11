18887a80e1ff05e0c6e7b43fbe7b6e262acebd68
Jonas Arndt <jonas.rogge@gmx.de>
2021-05-11 15:30:06 +0200

    Add new favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,10 +13,22 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
+
+      // favicon
+      { name: 'msapplication-TileColor', content: '#f9faff' },
+      { name: 'msapplication-config', content: './browserconfig.xml' },
+      { name: 'theme-color', content: '#ffffff' },
+
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: './favicon.ico' }
+      // favicon
+      { rel: 'apple-touch-icon', sizes: '180x180', href: './apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: './favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: './favicon-16x16.png' },
+      { rel: 'manifest', href: './site.webmanifest' },
+      { rel: 'mask-icon', href: './safari-pinned-tab.svg', color: '#ff4002' },
+      { rel: 'shortcut icon', href: './favicon.ico' },
     ]
   },
 
