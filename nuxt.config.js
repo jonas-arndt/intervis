865ead9d308ac3afd34a1470fa2aeeae1d69272c
Jonas Arndt <jonas.rogge@gmx.de>
2021-05-18 22:31:50 +0200

    Update fav icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,12 +23,12 @@ export default {
     ],
     link: [
       // favicon
-      { rel: 'apple-touch-icon', sizes: '180x180', href: './apple-touch-icon.png' },
-      { rel: 'icon', type: 'image/png', sizes: '32x32', href: './favicon-32x32.png' },
-      { rel: 'icon', type: 'image/png', sizes: '16x16', href: './favicon-16x16.png' },
-      { rel: 'manifest', href: './site.webmanifest' },
-      { rel: 'mask-icon', href: './safari-pinned-tab.svg', color: '#ff4002' },
-      { rel: 'shortcut icon', href: './favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: './favicon/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: './favicon/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: './favicon/favicon-16x16.png' },
+      { rel: 'manifest', href: './favicon/site.webmanifest' },
+      { rel: 'mask-icon', href: './favicon/safari-pinned-tab.svg', color: '#ff4002' },
+      { rel: 'shortcut icon', href: './favicon/favicon.ico' },
     ]
   },
 
@@ -50,6 +50,7 @@ export default {
       '~/components/disclosure',
       '~/components/elements',
       '~/components/fallback',
+      '~/components/instrumentation',
       '~/components/layout',
       '~/components/popups',
       '~/components/visualizations',
