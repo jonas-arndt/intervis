a2bca888253494bba548061614756e4b72c5e5e4
Jonas Arndt <jonas.rogge@gmx.de>
2021-05-20 16:37:48 +0200

    Replace scroll based chapter detection with intersection observer

--- components/ArticleContent.vue
+++ components/ArticleContent.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="article-content">
-    <Title />
-    <Introduction ref="introduction" @hook:mounted="updateChapterDimensions('introduction')" />
-    <Intersectionality ref="chapter1" @hook:mounted="updateChapterDimensions('chapter1')" />
-    <DiscriminationAndPrivilege ref="chapter2" @hook:mounted="updateChapterDimensions('chapter2')" />
-    <CaseStudies ref="chapter3" @hook:mounted="updateChapterDimensions('chapter3')" />
-    <Measures ref="chapter4" @hook:mounted="updateChapterDimensions('chapter4')" />
-    <NextSteps ref="chapter5" @hook:mounted="updateChapterDimensions('chapter5')" />
-    <ProjectInformation />
-    <References />
-    <Imprint />
+    <div id="introduction" class="intersecting">
+      <Title ref="title" />
+      <Introduction ref="introduction" @hook:mounted="updateChapterDimensions('introduction')" />
+    </div>
+    <Intersectionality id="chapter1" ref="chapter1" class="intersecting" @hook:mounted="updateChapterDimensions('chapter1')" />
+    <DiscriminationAndPrivilege id="chapter2" ref="chapter2" class="intersecting" @hook:mounted="updateChapterDimensions('chapter2')" />
+    <CaseStudies id="chapter3" ref="chapter3" class="intersecting" @hook:mounted="updateChapterDimensions('chapter3')" />
+    <Measures id="chapter4" ref="chapter4" class="intersecting" @hook:mounted="updateChapterDimensions('chapter4')" />
+    <div id="chapter5" class="intersecting">
+      <NextSteps ref="chapter5" @hook:mounted="updateChapterDimensions('chapter5')" />
+      <ProjectInformation />
+      <References />
+      <Imprint />
+    </div>
   </div>
 </template>
 
@@ -19,40 +23,22 @@ import { mapMutations, mapState } from 'vuex'
 export default {
   data () {
     return {
-      chapterDimensions: {},
-      activeChapterDimensions: undefined
+      intersectionObserverConfig: {
+        root: this.$el, // setting root to null sets it to viewport
+        rootMargin: '0px',
+        threshold: 0
+      },
+      observer: undefined
     }
   },
   computed: {
-    ...mapState(['verticalScrollPosition', 'verticalViewportCenter', 'activeArticleChapterId']),
-    centeredScrollPosition () {
-      return Math.max(0, this.verticalScrollPosition - this.verticalViewportCenter)
-    }
-  },
-  watch: {
-    centeredScrollPosition (scrollPosition) {
-      // if current chapter is undefined or not active anymore
-      if (this.activeArticleChapterId === undefined || scrollPosition < this.activeChapterDimensions.y1 || scrollPosition > this.activeChapterDimensions.y2) {
-        for (const [chapterId, dimensions] of Object.entries(this.chapterDimensions)) {
-          if (scrollPosition >= dimensions.y1 && scrollPosition <= dimensions.y2) {
-            // TODO: Update chapter dimensions when page is completely loaded
-            this.updateAllChapterDimensions()
-
-            this.setActiveArticleChapterId(chapterId)
-            this.activeChapterDimensions = dimensions
-            return
-          }
-        }
-
-        // reset values if no matching chapter was found
-        this.setActiveArticleChapterId(undefined)
-        this.activeChapterDimensions = undefined
-      }
-    }
+    ...mapState(['verticalScrollPosition'])
   },
   mounted () {
     this.$nuxt.$on('windowResized', this.handleWindowResizeEvent)
     this.$nuxt.$on('scrollToChapter', this.handleCallToChapterEvent)
+
+    this.addIntersectionObserver()
   },
   methods: {
     ...mapMutations([
@@ -62,11 +48,33 @@ export default {
       'setDiscriminationChapterStartPosition',
       'setNextStepsChapterStartPosition'
     ]),
+    addIntersectionObserver () {
+      const self = this
+      this.observer = new IntersectionObserver(
+        this.handleIntersectionObserverCallback,
+        this.intersectionObserverConfig
+      )
+
+      const domElements = document.querySelectorAll('.intersecting')
+      domElements.forEach((domElement) => {
+        self.observer.observe(domElement)
+      })
+    },
+    handleIntersectionObserverCallback (entries) {
+      const self = this
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          self.handleElementIntersection(entry)
+        }
+      })
+    },
+    handleElementIntersection (entry) {
+      this.setActiveArticleChapterId(entry.target.id)
+    },
     handleCallToChapterEvent (chapterId) {
-      if (chapterId in this.chapterDimensions) {
-        this.updateChapterDimensions(chapterId)
-        this.$nuxt.$emit('scrollArticle', this.chapterDimensions[chapterId].y1)
-      }
+      console.log(chapterId)
+      const rect = this.$refs[chapterId].$el.getBoundingClientRect()
+      this.$nuxt.$emit('scrollArticle', this.verticalScrollPosition + rect.top)
     },
     handleWindowResizeEvent () {
       this.updateAllChapterDimensions()
@@ -78,27 +86,24 @@ export default {
     },
     updateChapterDimensions (chapterId) {
       const boundingClientRect = this.$refs[chapterId].$el.getBoundingClientRect()
-      const y1 = this.verticalScrollPosition + Math.round(boundingClientRect.top)
-      const y2 = y1 + Math.round(boundingClientRect.height)
-
-      this.chapterDimensions[chapterId] = { y1, y2 }
+      const y = this.verticalScrollPosition + Math.round(boundingClientRect.top)
 
       // set break point values
       switch (chapterId) {
         case 'introduction': {
-          this.setIntroductionStartPosition(y1)
+          this.setIntroductionStartPosition(y)
           break
         }
         case 'chapter1': {
-          this.setIntersectionalityChapterStartPosition(y1)
+          this.setIntersectionalityChapterStartPosition(y)
           break
         }
         case 'chapter2': {
-          this.setDiscriminationChapterStartPosition(y1)
+          this.setDiscriminationChapterStartPosition(y)
           break
         }
         case 'chapter5': {
-          this.setNextStepsChapterStartPosition(y1)
+          this.setNextStepsChapterStartPosition(y)
           break
         }
       }
