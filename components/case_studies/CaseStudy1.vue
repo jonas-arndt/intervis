30563292e27e49fe7da6add7a6dc7548fb49c8b1
Jonas Arndt <jonas.rogge@gmx.de>
2021-05-23 11:12:28 +0200

    Add workaround for case study detection

--- components/case_studies/CaseStudy1.vue
+++ components/case_studies/CaseStudy1.vue
@@ -1,30 +1,32 @@
 <template>
-  <CaseStudyContainer class="case-study working-women">
-    <template v-slot:background-visualization>
-      <WorkingWomanVisualization />
-    </template>
+  <div>
+    <CaseStudyContainer class="case-study working-women">
+      <template v-slot:background-visualization>
+        <WorkingWomanVisualization />
+      </template>
 
-    <template v-slot:content>
-      <div class="step">
-        <h3 class="case-study-title" v-html="$t('chapter3-example1-title')" />
-        <p v-html="$t('chapter3-example1-text1')" />
-      </div>
+      <template v-slot:content>
+        <div class="step">
+          <h3 class="case-study-title" v-html="$t('chapter3-example1-title')" />
+          <p v-html="$t('chapter3-example1-text1')" />
+        </div>
 
-      <div class="step">
-        <p v-html="$t('chapter3-example1-text2')" />
-        <blockquote v-html="$t('chapter3-example1-trend')" />
-        <p v-html="$t('chapter3-example1-text3')" />
-      </div>
+        <div class="step">
+          <p v-html="$t('chapter3-example1-text2')" />
+          <blockquote v-html="$t('chapter3-example1-trend')" />
+          <p v-html="$t('chapter3-example1-text3')" />
+        </div>
 
-      <div class="step">
-        <p v-html="$t('chapter3-example1-text4')" />
-      </div>
+        <div class="step">
+          <p v-html="$t('chapter3-example1-text4')" />
+        </div>
 
-      <div class="step">
-        <p v-html="$t('chapter3-example1-text5')" />
-      </div>
-    </template>
-  </CaseStudyContainer>
+        <div class="step">
+          <p v-html="$t('chapter3-example1-text5')" />
+        </div>
+      </template>
+    </CaseStudyContainer>
+  </div>
 </template>
 
 <style lang="scss" scoped>
--- components/case_studies/CaseStudy3.vue
+++ components/case_studies/CaseStudy3.vue
@@ -1,30 +1,32 @@
 <template>
-  <CaseStudyContainer class="case-study working-women">
-    <template v-slot:background-visualization>
-      <WorkingHandicappedVisualization />
-    </template>
+  <div>
+    <CaseStudyContainer class="case-study working-women">
+      <template v-slot:background-visualization>
+        <WorkingHandicappedVisualization />
+      </template>
 
-    <template v-slot:content>
-      <div class="step">
-        <h3 class="case-study-title" v-html="$t('chapter3-example3-title')" />
-        <p v-html="$t('chapter3-example3-text1')" />
-      </div>
+      <template v-slot:content>
+        <div class="step">
+          <h3 class="case-study-title" v-html="$t('chapter3-example3-title')" />
+          <p v-html="$t('chapter3-example3-text1')" />
+        </div>
 
-      <div class="step">
-        <p v-html="$t('chapter3-example3-text2')" />
-      </div>
+        <div class="step">
+          <p v-html="$t('chapter3-example3-text2')" />
+        </div>
 
-      <div class="step">
-        <p v-html="$t('chapter3-example3-text3')" />
-      </div>
+        <div class="step">
+          <p v-html="$t('chapter3-example3-text3')" />
+        </div>
 
-      <div class="step">
-        <p v-html="$t('chapter3-example3-text4')" />
-        <blockquote v-html="$t('chapter3-example3-trend')" />
-        <p v-html="$t('chapter3-example3-text5')" />
-      </div>
-    </template>
-  </CaseStudyContainer>
+        <div class="step">
+          <p v-html="$t('chapter3-example3-text4')" />
+          <blockquote v-html="$t('chapter3-example3-trend')" />
+          <p v-html="$t('chapter3-example3-text5')" />
+        </div>
+      </template>
+    </CaseStudyContainer>
+  </div>
 </template>
 
 <style lang="scss" scoped>
