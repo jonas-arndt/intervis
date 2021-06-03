d3d1155dd9a5464a254983fabb433f5a85d8956e
Marian Dörk <doerk@fh-potsdam.de>
2021-06-03 10:34:44 +0200

    lower-case i

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
 			// preview
 			{ property: 'og:type', content:'website' },
 			{ property: 'og:url', content:`https://uclab.fh-potsdam.de${parentDirname}/` },
-			{ property: 'og:title', content:'Inter...what? Intersectionality! A visual Introduction' },
+			{ property: 'og:title', content:'Inter...what? Intersectionality! A visual introduction' },
 			{ property: 'og:description', content:'With this article we aim to accessibly explain the topics of intersectionality, discrimination and privilege using visual elements. Intersectional perspectives have not yet arrived in everyday vocabulary and social discourse in Germany. We would like to change that, and this article can be a beginning for that.' },
 			{ property: 'og:image', content: `https://uclab.fh-potsdam.de${parentDirname}/preview.png` },
 			{ name: 'twitter:card', content:'summary_large_image' },
